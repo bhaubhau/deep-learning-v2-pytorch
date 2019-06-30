e402c968680b0c817b668f55e105df35c552816e
Bhavik Kawli <bhavik.kawli@gmail.com>
2019-06-30 14:19:01 +0000

    added git setup file

---
+++ git_setup.sh
@@ -0,0 +1,6 @@
+git remote set-url origin https://github.com/bhaubhau/deep-learning-v2-pytorch.git
+git config --global user.name "Bhavik Kawli"
+git config --global user.email "bhavik.kawli@gmail.com"
+git remote get-url origin
+git config --get user.name
+git config --get user.email
